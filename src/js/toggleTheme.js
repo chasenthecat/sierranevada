7ffa491d028311ea1a994a170d00f0d73380c25f
JoseGonzalez <70743629+JoseGonzalez-git@users.noreply.github.com>
2021-11-23 14:03:39 -0500

    update toggleTheme.js

--- src/js/toggleTheme.js
+++ src/js/toggleTheme.js
@@ -1,5 +1,23 @@
-let toggleTheme = document.querySelector("#theme-toggler");
+let theme_toggler = document.querySelector('#theme-toggler');
 
-toggleTheme.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+theme_toggler.addEventListener('click', function(){ 
+    document.body.classList.toggle('dark-mode');
+  if(document.body.classList.contains('dark-mode')){
+ localStorage.setItem('website_theme','dark-mode');
+  }else{
+    localStorage.setItem('website_theme','default');
+  }
 });
+
+function retrieve_theme(){
+  var theme = localStorage.getItem('website_theme');
+  if(theme != null){
+ document.body.classList.remove('default', 'dark-mode'); document.body.classList.add(theme);
+  }
+}
+
+retrieve_theme();
+
+window.addEventListener("storage",function(){
+retrieve_theme();
+},false);

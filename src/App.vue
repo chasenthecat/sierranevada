e324193e862dda7a4d7a37e4314ca2a2df720939
carlosarguelles <carlosnapx@gmail.com>
2021-11-29 22:02:19 -0500

    feat: first commit using Vue 3 + Vite + TypeScript

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="flex h-screen">
+    <div class="m-auto text-center">
+      <h1 class="font-bold text-8xl">Hello world from Vue</h1>
+      <h2 class="mt-5 text-6xl">Cuasi Sharks 🤑</h2>
+    </div>
+  </main>
+</template>
---
+++ src/components/Index.vue
@@ -0,0 +1,3 @@
+<template>
+<div class="te">Hello</div>
+</template>

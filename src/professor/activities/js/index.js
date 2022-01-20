46a845001b5ee6a3225e93ce458577d15ac5be2f
carlosarguelles <carlosnapx@gmail.com>
2022-01-20 17:09:48 -0500

    added cards for courses in student module

--- src/professor/activities/js/index.js
+++ src/professor/activities/js/index.js
@@ -1,188 +1,185 @@
-
 function formOnChange(select) {
-    if (select.value == 'crear') {
+  if (select.value == 'crear') {
+    divActivity = document.getElementById('div-actividad')
+    divActivity.style.display = ''
 
-        divActivity = document.getElementById('div-actividad')
-        divActivity.style.display = ''
+    divLevelGroup = document.getElementById('div-level-group')
+    divLevelGroup.style.display = ''
 
-        divLevelGroup = document.getElementById('div-level-group')
-        divLevelGroup.style.display = ''
+    divMaterial = document.getElementById('div-materia')
+    divMaterial.style.display = ''
 
-        divMaterial = document.getElementById('div-materia')
-        divMaterial.style.display = ''
+    divBtn1 = document.getElementById('btn1')
+    divBtn1.style.display = ''
 
-        divBtn1 = document.getElementById('btn1')
-        divBtn1.style.display = ''
+    divData = document.getElementById('data')
+    divData.style.display = 'none'
+  } else {
+    divActivity = document.getElementById('div-actividad')
+    divActivity.style.display = 'none'
 
-        divData = document.getElementById('data')
-        divData.style.display = 'none'
+    divLevelGroup = document.getElementById('div-level-group')
+    divLevelGroup.style.display = 'none'
 
-    } else {
-        divActivity = document.getElementById('div-actividad')
-        divActivity.style.display = 'none'
+    divMaterial = document.getElementById('div-materia')
+    divMaterial.style.display = 'none'
 
-        divLevelGroup = document.getElementById('div-level-group')
-        divLevelGroup.style.display = 'none'
+    divBtn1 = document.getElementById('btn1')
+    divBtn1.style.display = 'none'
 
-        divMaterial = document.getElementById('div-materia')
-        divMaterial.style.display = 'none'
+    divData = document.getElementById('data')
+    divData.style.display = ''
 
-        divBtn1 = document.getElementById('btn1')
-        divBtn1.style.display = 'none'
-
-        divData = document.getElementById('data')
-        divData.style.display = ''
-
-        listActivity(link_activity)
-    }
+    listActivity(link_activity)
+  }
 }
-
 
 function listGroup() {
-    const select = document.getElementById('levelGroup');
-    fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/group')
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                if (i == 0) {
-                    let option = document.createElement('option');
-                    option.value = '';
-                    option.innerHTML = 'Seleccionar un Grupo';
-                    select.appendChild(option);
-                }
-                let option = document.createElement('option');
-                option.value = data[i].level;
-                option.innerHTML = data[i].level;
-                select.appendChild(option);
-            }
-        });
+  const select = document.getElementById('levelGroup')
+  fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/group')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (i == 0) {
+          let option = document.createElement('option')
+          option.value = ''
+          option.innerHTML = 'Seleccionar un Grupo'
+          select.appendChild(option)
+        }
+        let option = document.createElement('option')
+        option.value = data[i].level
+        option.innerHTML = data[i].level
+        select.appendChild(option)
+      }
+    })
 }
-const link_activity = 'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity'
+const link_activity =
+  'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity'
 
 function listActivity(link_activity) {
-    const tbody = document.getElementById('list');
-    fetch(link_activity)
-        .then(response => response.json())
-        .then(data => {
-            tbody.innerHTML = '';
-            for (let i = 0; i < data.length; i++) {
-                let fila = tbody.insertRow();
-                fila.insertCell().innerHTML = data[i].descripcion;
-                fila.insertCell().innerHTML = data[i].materia;
-                fila.insertCell().innerHTML = data[i].grupo;
-                fila.insertCell().innerHTML = `
+  const tbody = document.getElementById('list')
+  fetch(link_activity)
+    .then((response) => response.json())
+    .then((data) => {
+      tbody.innerHTML = ''
+      for (let i = 0; i < data.length; i++) {
+        let fila = tbody.insertRow()
+        fila.insertCell().innerHTML = data[i].descripcion
+        fila.insertCell().innerHTML = data[i].materia
+        fila.insertCell().innerHTML = data[i].grupo
+        fila.insertCell().innerHTML = `
             <button onclick="goedit(${data[i].id})" type="button">Editar</button> | <button type="button" onclick="deleteActivity(${data[i].id})" >Borrar</button>
-            `;
-            }
-        });
+            `
+      }
+    })
 }
 
 function deleteActivity(id) {
-    fetch(link_activity + `/${id}`, {
-        method: 'DELETE',
-    })
-        .then((response) => {
-            console.log(response.status);
-            if (response.status == 200) {
-                alert('Se ha eliminado correctamente');
-            }
-        })
+  fetch(link_activity + `/${id}`, {
+    method: 'DELETE',
+  }).then((response) => {
+    console.log(response.status)
+    if (response.status == 200) {
+      alert('Se ha eliminado correctamente')
+    }
+  })
 }
 
 function listMateria() {
-    const select = document.getElementById('pMateria');
-    fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/course')
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                if (i == 0) {
-                    let option = document.createElement('option');
-                    option.value = '';
-                    option.innerHTML = 'Seleccionar un Materia';
-                    select.appendChild(option);
-                }
-                let option = document.createElement('option');
-                option.value = data[i].title;
-                option.innerHTML = data[i].title;
-                select.appendChild(option);
-            }
-        });
+  const select = document.getElementById('pMateria')
+  fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/course')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (i == 0) {
+          let option = document.createElement('option')
+          option.value = ''
+          option.innerHTML = 'Seleccionar un Materia'
+          select.appendChild(option)
+        }
+        let option = document.createElement('option')
+        option.value = data[i].title
+        option.innerHTML = data[i].title
+        select.appendChild(option)
+      }
+    })
 }
 
 const formActividad = {
-    descripcion: document.getElementById('actividad'),
-    grupo: document.getElementById('levelGroup'),
-    materia: document.getElementById('pMateria'),
+  descripcion: document.getElementById('actividad'),
+  grupo: document.getElementById('levelGroup'),
+  materia: document.getElementById('pMateria'),
 }
 
-
 function validateActivity(args) {
-    if (args.descripcion === '') {
-        alert('No puede dejar el campo de identificación vacío')
-        return false
-    }
-    if (args.materia === '') {
-        alert('No puede dejar el campo de nombre vacío')
-        return false
-    }
-    if (args.grupo === '') {
-        alert('No puede dejar el campo de apellido vacío')
-        return false
-    }
-    if (args.idprofesor === '') {
-        alert('No puede dejar el campo de correo vacío')
-        return false
-    }
-    return true
+  if (args.descripcion === '') {
+    alert('No puede dejar el campo de identificación vacío')
+    return false
+  }
+  if (args.materia === '') {
+    alert('No puede dejar el campo de nombre vacío')
+    return false
+  }
+  if (args.grupo === '') {
+    alert('No puede dejar el campo de apellido vacío')
+    return false
+  }
+  if (args.idprofesor === '') {
+    alert('No puede dejar el campo de correo vacío')
+    return false
+  }
+  return true
 }
 
 async function addActivity() {
-
-    let data = {
-        descripcion: formActividad.descripcion.value,
-        materia: formActividad.materia.value,
-        grupo: formActividad.grupo.value,
-        idprofesor: sessionStorage.getItem('user')
-    }
-    let response
-    try {
-        if (!validateActivity(data)) {
-            throw new Error('Corrige los datos.')
-        } else {
-            const body = JSON.stringify(data)
-            response = await fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-                body,
-            })
+  let data = {
+    descripcion: formActividad.descripcion.value,
+    materia: formActividad.materia.value,
+    grupo: formActividad.grupo.value,
+    idprofesor: sessionStorage.getItem('user'),
+  }
+  let response
+  try {
+    if (!validateActivity(data)) {
+      throw new Error('Corrige los datos.')
+    } else {
+      const body = JSON.stringify(data)
+      response = await fetch(
+        'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+          body,
         }
-    } catch (err) {
-        alert(err)
+      )
     }
+  } catch (err) {
+    alert(err)
+  }
 
-    if (response?.status === 201) {
-        alert('Actividad Agregada')
-        window.location.href = 'index.html'
-    }
+  if (response?.status === 201) {
+    alert('Actividad Agregada')
+    window.location.href = 'index.html'
+  }
 }
 
 document.getElementById('btn-submit').addEventListener('click', async (e) => {
-    e.preventDefault()
-    await addActivity()
+  e.preventDefault()
+  await addActivity()
 })
 
-
-
 function goedit(id) {
-    window.location.href = 'editar.html?id=' + id
+  window.location.href = 'editar.html?id=' + id
 }
 
 function updateActivity(id) {
-    console.log(id)
-    fetch('https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity' + `/${id}`, {
+  console.log(id)
+  fetch(
+    'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity' + `/${id}`,
+    {
       method: 'PUT',
       headers: {
         Accept: 'application/json, text/plain, */*',
@@ -193,16 +190,17 @@ function updateActivity(id) {
         grupo: formActividad.grupo.value,
         materia: formActividad.materia.value,
       }),
+    }
+  )
+    .then((response) => {
+      if (response.status == 200) {
+        alert('Se ha actualizado correctamente')
+        window.location.href = './index.html'
+      }
     })
-      .then((response) => {
-        if (response.status == 200) {
-          alert('Se ha actualizado correctamente')
-          window.location.href = './index.html'
-        }
-      })
-      .catch((err) => {
-        console.log(err)
-        alert('Error al actualizar')
-          window.location.href = './index.html'
-      })
-  }
+    .catch((err) => {
+      console.log(err)
+      alert('Error al actualizar')
+      window.location.href = './index.html'
+    })
+}
--- src/student/js/activities.js
+++ src/student/js/activities.js
@@ -1,42 +1,53 @@
-
 window.onload = function () {
-    const link_activity = 'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity';
-    listActivities(link_activity);
+  const link_activity =
+    'https://61cd1a30198df60017aec2d4.mockapi.io/api/v1/activity'
+  listActivities(link_activity)
+}
+
+function insertCard(item) {
+  let divActivity = document.getElementById('activities')
+  divActivity.innerHTML += `
+                    <section class="card">
+                    <h3><a href="#">${item?.descripcion}</a></h3>
+                    <p>Plazo hasta: <em>11:59 p.m.</em></p>
+                    <p>Grupo: <em>${item?.grupo}</em></p>
+                    <p>Asignatura: <em>${item?.materia}</em></p>
+                    </section>`
 }
 
 async function listActivities(link_activity) {
+  let response
 
-    let response
-    try {
-        response = await fetch(link_activity)
+  try {
+    response = await fetch(link_activity).then((response) => response.json())
+  } catch (error) {
+    alert('Error al cargar las actividades')
+  }
 
-    } catch (error) {
-        alert('Error al cargar las actividades');
-    }
+  const group = sessionStorage.getItem('group')
 
-    const group = sessionStorage.getItem('group');
+  console.log(response)
 
-    response.json().then(data => {
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            let number_activities = document.getElementById('number_activities');
-            let num
-            if (group === data[i].grupo) {
-                let divActivity = document.getElementById('activities');
-                divActivity.innerHTML += `
-                    <section class="card" >
-                    <h3><a href="#">${data[i].descripcion}</a></h3>
-                    <p>Plazo hasta: <em>11:59 p.m.</em></p>
-                    <p>Grupo: <em>${data[i].grupo}</em></p>
-                    <p>Asignatura: <em>${data[i].materia}</em></p>
-                    </section>
-            `;
-                num = i + 1;
-            }
-            else {
-                num = 0;
-            }
-            number_activities.innerHTML = num;
-        }
-    });
+  const activitiesByGroup = response.filter(
+    (activity) => activity.grupo === group
+  )
+
+  const activityIndicator = document.getElementById('number_activities')
+
+  if (activitiesByGroup.length === 0) {
+    activityIndicator.style.display = 'none'
+  } else {
+    activitiesByGroup.forEach((activity) => insertCard(activity))
+    activityIndicator.innerHTML = activitiesByGroup.length
+  }
 }
+
+document.getElementById('date').innerHTML = new Date().toLocaleDateString(
+  'es-CO',
+  {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+)
